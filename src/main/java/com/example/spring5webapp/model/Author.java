ff0211c1fa1b3ae319f29ce4be302c7f78924f58
felleet <felleet@gmail.com>
2018-06-30 14:48:10 +0300

    [+] jpa entities start. Authors and books

--- src/main/java/com/example/spring5webapp/model/Author.java
+++ src/main/java/com/example/spring5webapp/model/Author.java
@@ -1,14 +1,20 @@
 package com.example.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
 
