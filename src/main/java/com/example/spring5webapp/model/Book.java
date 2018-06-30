e77cab0c73c1564c6d804cb67591dde1622e2e9f
felleet <felleet@gmail.com>
2018-06-30 14:15:51 +0300

    [+] initial commit

---
+++ src/main/java/com/example/spring5webapp/model/Book.java
@@ -0,0 +1,15 @@
+package com.example.spring5webapp.model;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+}
