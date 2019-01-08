c0e9a6ebc9b638e3d817f644eb0584103316c28d
felleet <felleet@gmail.com>
2019-01-08 13:08:41 +0300

    [~] bean from context

--- src/main/java/com/example/spring5webapp/Spring5webappApplication.java
+++ src/main/java/com/example/spring5webapp/Spring5webappApplication.java
@@ -1,12 +1,19 @@
 package com.example.spring5webapp;
 
+import com.example.spring5webapp.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Spring5webappApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		String hello = controller.hello();
+		System.out.println(hello);
 	}
 }
